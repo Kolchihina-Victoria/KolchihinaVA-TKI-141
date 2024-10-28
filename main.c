9a0fc62d834855e14631d6fe367369d099f7f558
Колчихина Виктория <kolchihinavictoria@yandex.ru>
2024-10-28 17:05:04 +0300

    Update main.c

--- main.c
+++ main.c
@@ -1,6 +1,16 @@
 #include <stdio.h>
 #include <math.h>
+#include <stdlib.h>
+#include <stbool.h>
+#include <float.h>
 
+
+/**
+* @brief проверяет равно ли число нулю(через эпсилон)
+* @param number сравнимое число
+* @return true если число равно нулю,в противном случае false
+*/
+bool isZero(const double number);
 /*
 * @brief функция для вычисления значения y
 * @param x значение переменной х
@@ -13,6 +23,12 @@ double get_y(const double x);
 * @return введенное число типа double
 */
 double input(void);
+
+/**
+* @brief функция считывает ввод пользователя с проверкой на положительное число
+* @return введенное число типа double
+*/
+double Positiveinput(void);
 
 /*
 * @brief точка входа в программу
@@ -30,4 +46,26 @@ int main(void){
 }
 double get_y(const double x) {
 	return x + cos(pow(x, 0.52) + 2);
+}
+bool isZero(const double number)
+{
+  return (fabs(number) < DBL_EPSILON);
+}
+double input(void) {
+  double number = 0;
+  if (scanf_s("%lf", &number) != 1) {
+    puts("Ваш ввод некорректен");
+    exit(EXIT_FAILURE);
+  }
+  return number;
+}
+double positiveinput(void)
+{
+  double number = input();
+  if ((number < 0) || isZero(number))
+  {
+    puts("Введенное число меньше или равно нулю!");
+    exit(EXIT_FAILURE);
+  }
+  return number;
 }
