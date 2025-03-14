2386ac166a185fcbfb930c9b3fcc0da192069f5f
Колчихина Виктория <kolchihinavictoria@yandex.ru>
2025-03-15 00:10:49 +0300

    Update main.c

--- main.c
+++ main.c
@@ -1,78 +1,78 @@
-#include<stdio.h>
-#include<stdlib.h>
-#include<math.h>
-#include <stdbool.h>
+#include <iostream>
+#include <cmath>
 
-/**
- * @brief считывает вещественное число
- * @return вещественное число
- */
+using namespace std;
 
-    double Input(void);
-
-/**
- * @brief считывает вещественное число с проверкой знака
- * @return вещественное число в случае, если оно больше нуля
- */
-
-double PInput(void);
-
-/**
- * @brief определяет, пройдет ли кирпич в отверстие
- * @param x длина 1 стороны треугольника
- * @param y длина 2 стороны треугольника
- * @param z длина 3 стороны треугольника
- * @return true, если подходит и false, если не подходит
- */
-
-bool iflengthenter(const double x, const double y, const double z);
-
-/**
- * @brief точка входа в программу
- * @return 0 в случае успеха
- */
- 
-int main(void)
+class Coordinates 
 {
-    puts("Введите длину 1 стороны x треугольника ");
-    const double x = PInput();
-    puts("Введите длину 2 стороны y треугольника ");
-    const double y = PInput();
-    puts("Введите длину 3 стороны z треугольника ");
-    const double z = PInput();
-    if (iflengthenter(x, y, z) == true)
+    public:
+    double x, y, z;
+
+    Coordinates(double x = 0, double y = 0, double z = 0) : x(x), y(y), z(z) 
     {
-        puts("треугольник остроугольный");
-        return 0;
-    };
-    puts("треугольник неостроугольный");
+        
+    }
+    void newCoordinates(double newx,double newy,double newz)
+    {
+        x=newx;
+        y=newy;
+        z=newz;
+    }
+};
+class Formuls
+{
+    public:
+    double obem(double radius,double height)
+    {
+        return 3.14*(pow(radius,2))*height;
+    }
+    
+    
+   bool IsPointInside(Coordinates point, double radius,double height,Coordinates center)
+    {
+       double dx1=point.x;
+       double dy1=point.y;
+       double dz1=point.z;
+     
+     
+     bool condition1=(pow(dx1,2)+pow(dy1,2))<(pow(radius,2));
+     bool condition2=(-height/2.0<=dz1)&&(dz1<=height/2.0);
+     return condition1&&condition2;
+    
+    
+    }
+};
+int main()
+{
+    Formuls calculator;
+    double radius, height;
+    Coordinates center;
+   
+    
+    cout << "Введите координаты центра основания цилиндра : ";
+    cin >> center.x >> center.y >> center.z;
+    cout << "Введите радиус цилиндра: ";
+    cin >> radius;
+    cout << "Введите высоту цилиндра: ";
+    cin >> height;
+    
+    double obem = calculator.obem(radius, height);
+    cout << "Объем цилиндра: " << obem << endl;
+    
+    
+      
+    Coordinates point;
+    cout << "Введите координаты точки : ";
+    cin >> point.x >>point.y >> point.z;
+
+    bool isInside = calculator.IsPointInside(point, radius, height, center);
+
+    if (isInside) {
+        cout << "Точка лежит внутри цилиндра." << endl;
+    } else {
+        cout << "Точка не лежит внутри цилиндра." << endl;
+    }
+
     return 0;
-}
-double Input(void)
-{
-    double value = 0.0;
-    if (scanf_s("%lf", &value) != 1)
-    {
-        puts("Не является числом");
-        exit(EXIT_FAILURE);
-    }
-    return value;
-}
-double PInput(void)
-{
-    double value = Input();
-    if (value <= 0)
-    {
-        puts("Некорректное значение длины");
-        exit(EXIT_FAILURE);
-    }
-    return value;
-}
-bool iflengthenter(const double x, const double y, const double z)
-{
-    if (pow(x,2)<(pow(y,2)+pow(z,2)))
-    {
-        return true;
-    }
-    return false;
+
 }
