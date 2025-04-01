fe1edd1fbd248184564a3dcb745c1b80c2c7bde6
Колчихина Виктория <kolchihinavictoria@yandex.ru>
2025-04-01 21:18:16 +0300

    Update main.c

--- main.c
+++ main.c
@@ -1,78 +1,121 @@
 #include <iostream>
+#include <string>
 #include <cmath>
+#include <stdexcept>
 
-using namespace std;
+// Определение класса Point
+class Point {
+private:
+    double x;
+    double y;
+    double z;
 
-class Coordinates 
-{
-    public:
-    double x, y, z;
-
-    Coordinates(double x = 0, double y = 0, double z = 0) : x(x), y(y), z(z) 
-    {
-        
-    }
-    void newCoordinates(double newx,double newy,double newz)
-    {
-        x=newx;
-        y=newy;
-        z=newz;
-    }
+public:
+    Point(double x = 0.0, double y = 0.0, double z = 0.0);
+    double getX() const;
+    double getY() const;
+    double getZ() const;
 };
-class Formuls
-{
-    public:
-    double obem(double radius,double height)
-    {
-        return 3.14*(pow(radius,2))*height;
-    }
-    
-    
-   bool IsPointInside(Coordinates point, double radius,double height,Coordinates center)
-    {
-       double dx1=point.x;
-       double dy1=point.y;
-       double dz1=point.z;
-     
-     
-     bool condition1=(pow(dx1,2)+pow(dy1,2))<(pow(radius,2));
-     bool condition2=(-height/2.0<=dz1)&&(dz1<=height/2.0);
-     return condition1&&condition2;
-    
-    
-    }
+
+// Реализация методов класса Point
+Point::Point(double x, double y, double z) : x(x), y(y), z(z) {}
+
+double Point::getX() const {
+    return x;
+}
+
+double Point::getY() const {
+    return y;
+}
+
+double Point::getZ() const {
+    return z;
+}
+
+// Определение класса Cylindr
+class Cylindr {
+private:
+    Point center;
+    double radius;
+    double height;
+
+public:
+    Cylindr(const Point& center, double radius, double height);
+    double getRadius() const;
+    double getHeight() const;
+    double getVolume() const;
+    bool isPointInside(const Point& point) const;
 };
-int main()
-{
-    Formuls calculator;
-    double radius, height;
-    Coordinates center;
-   
-    
-    cout << "Введите координаты центра основания цилиндра : ";
-    cin >> center.x >> center.y >> center.z;
-    cout << "Введите радиус цилиндра: ";
-    cin >> radius;
-    cout << "Введите высоту цилиндра: ";
-    cin >> height;
-    
-    double obem = calculator.obem(radius, height);
-    cout << "Объем цилиндра: " << obem << endl;
-    
-    
-      
-    Coordinates point;
-    cout << "Введите координаты точки : ";
-    cin >> point.x >>point.y >> point.z;
 
-    bool isInside = calculator.IsPointInside(point, radius, height, center);
+// Реализация методов класса Cylindr
+const double PI = 3.14159;
 
-    if (isInside) {
-        cout << "Точка лежит внутри цилиндра." << endl;
+Cylindr::Cylindr(const Point& center, double radius, double height) :
+    center(center), radius(radius), height(height) {}
+
+double Cylindr::getRadius() const {
+    return radius;
+}
+
+double Cylindr::getHeight() const {
+    return height;
+}
+
+double Cylindr::getVolume() const {
+    return PI * radius * radius * height;
+}
+
+bool Cylindr::isPointInside(const Point& point) const {
+    double dx = point.getX() - center.getX();
+    double dy = point.getY() - center.getY();
+    double dz = point.getZ() - center.getZ();
+
+    if (dx * dx + dy * dy > radius * radius) {
+        return false;
+    }
+
+    if (dz < 0 || dz > height) {
+        return false;
+    }
+
+    return true;
+}
+
+// Прототипы функций
+double getCoordinate(std::string message);
+Point getPoint();
+
+int main() {
+    Point cylindrCenter = getPoint();
+    double cylindrRadius = getCoordinate("enter the radius: ");
+    double cylindrHeight = getCoordinate("enter the height: ");
+
+    Cylindr cylindr(cylindrCenter, cylindrRadius, cylindrHeight);
+    std::cout << "volume: " << cylindr.getVolume() << std::endl;
+    std::cout << "enter coordinates for check:" << std::endl;
+    Point checkPoint = getPoint();
+    if (cylindr.isPointInside(checkPoint)) {
+        std::cout << "point inside." << std::endl;
     } else {
-        cout << "Точка не лежит внутри цилиндра." << endl;
+        std::cout << "point outside." << std::endl;
     }
 
     return 0;
+}
 
+double getCoordinate(std::string message) {
+    std::cout << message << std::endl;
+    double coordinate = 0.0;
+    std::cin >> coordinate;
+    if (std::cin.fail()) {
+        throw std::invalid_argument("Your input is uncorrected");
+    }
+    return coordinate;
+}
+
+Point getPoint() {
+    double x = getCoordinate("enter coordinate x: ");
+    double y = getCoordinate("enter coordinate y: ");
+    double z = getCoordinate("enter coordinate z: ");
+    return Point(x, y, z);
 }
