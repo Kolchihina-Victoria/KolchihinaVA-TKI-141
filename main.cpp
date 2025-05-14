be031600157744a284776673c44f32780d629fd1
Колчихина Виктория <kolchihinavictoria@yandex.ru>
2025-05-14 15:16:26 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -4,10 +4,28 @@
 
 /**
  * @brief Основная функция программы
- * 
- * Демонстрирует работу с классами Piquet и SpeedRestriction
+ * @return Код завершения программы
  */
 int main() {
-    // Пример использования классов...
+    // Пример использования класса Piquet
+    Piquet p1(12, 28.37);
+    Piquet p2(-5, 50.0);
+    std::cout << "Пикет 1: " << p1 << std::endl;
+    std::cout << "Пикет 2: " << p2 << std::endl;
+
+    if (p1 > p2) {
+        std::cout << "Пикет 1 больше пикета 2" << std::endl;
+    } else {
+        std::cout << "Пикет 1 не больше пикета 2" << std::endl;
+    }
+
+    // Пример использования класса SpeedRestriction
+    Restriction* restriction = SpeedRestriction::ReadFromStream(std::cin);
+
+    if (restriction) {
+        std::cout << "Введенное ограничение: " << *restriction << std::endl;
+        delete restriction;
+    }
+
     return 0;
 }
