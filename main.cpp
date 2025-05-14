0d4dc32c08b954123b69cc7acc36471529b25886
Колчихина Виктория <kolchihinavictoria@yandex.ru>
2025-05-14 15:05:54 +0300

    Задание 3

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include "piquet.h"
+#include "speed_restriction.h"
+
+/**
+ * @brief Основная функция программы
+ * 
+ * Демонстрирует работу с классами Piquet и SpeedRestriction
+ */
+int main() {
+    // Пример использования классов...
+    return 0;
+}
