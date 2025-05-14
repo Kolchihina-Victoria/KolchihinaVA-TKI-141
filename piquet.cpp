1d2fe9134c2abdd96c0e50ba3b27042af789acbe
Колчихина Виктория <kolchihinavictoria@yandex.ru>
2025-05-14 15:15:56 +0300

    Update piquet.cpp

--- piquet.cpp
+++ piquet.cpp
@@ -1,20 +1,27 @@
 #include "piquet.h"
 
-Piquet::Piquet(int num, double sh, double len, bool standard) 
+/**
+ * @brief Конструктор пикета
+ */
+Piquet::Piquet(int num, double sh, double len, bool standard)
     : number(num), shift(sh), length(len), isStandard(standard) {}
 
+/**
+ * @brief Преобразует пикет в строку
+ */
 std::string Piquet::ToString() const {
     std::stringstream ss;
-    ss << "ПК " << std::setfill('0');
+    ss << "ПК ";
     if (number >= 0) {
         ss << number;
     } else {
-        ss << "0" << -number;
+        ss << "0" << -number; // Для отрицательных точек
     }
     ss << "+" << std::fixed << std::setprecision(2) << shift;
     return ss.str();
 }
 
+// Реализации операторов сравнения
 bool Piquet::operator==(const Piquet& other) const {
     return (number == other.number && shift == other.shift);
 }
@@ -29,15 +36,31 @@ bool Piquet::operator<(const Piquet& other) const {
     return false;
 }
 
-// Остальные операторы сравнения...
+bool Piquet::operator>(const Piquet& other) const {
+    return other < *this;
+}
 
+bool Piquet::operator<=(const Piquet& other) const {
+    return !(other < *this);
+}
+
+bool Piquet::operator>=(const Piquet& other) const {
+    return !(*this < other);
+}
+
+/**
+ * @brief Оператор вывода пикета в поток
+ */
 std::ostream& operator<<(std::ostream& os, const Piquet& p) {
     os << p.ToString();
     return os;
 }
 
+/**
+ * @brief Оператор ввода пикета из потока
+ */
 std::istream& operator>>(std::istream& is, Piquet& p) {
-    char pk, plus;
+    char pk;
     is >> pk; // Читаем 'П'
     if (pk != 'П') {
         is.setstate(std::ios::failbit);
@@ -48,9 +71,15 @@ std::istream& operator>>(std::istream& is, Piquet& p) {
         is.setstate(std::ios::failbit);
         return is;
     }
-    is >> p.number >> plus >> p.shift; // Читаем номер и смещение
+    is >> p.number;
+
+    char plus;
+    is >> plus; // Читаем '+'
     if (plus != '+') {
         is.setstate(std::ios::failbit);
+        return is;
     }
-    return is; // Обязательный возврат потока!
+
+    is >> p.shift;
+    return is;
 }
