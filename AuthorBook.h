7c7b3e2053ad4d5135efbf1d6835047da98e0735
Колчихина Виктория <kolchihinavictoria@yandex.ru>
2025-12-04 14:52:00 +0300

    Update AuthorBook.h

--- AuthorBook.h
+++ AuthorBook.h
@@ -1,34 +1,39 @@
 #pragma once
 // AuthorBook.h - Заголовочный файл класса AuthorBook
 // Назначение: Определяет класс для книг с одним автором
-// Наследует: Класс Book
-// Особенность: Реализует чисто виртуальные методы базового класса
 
-#include "Book.h"  // Базовый класс
+#include "Book.h"
+#include "Author.h"
 #include <string>
 #include <vector>
+#include <memory>
 
 // Класс для представления книги с одним автором
 // Наследует все поля и методы от Book
 class AuthorBook : public Book {
 private:
-    // Дополнительное поле, специфичное для этого класса
-    std::string author;  // Единственный автор книги
-    
+    std::shared_ptr<Author> author;  // Указатель на автора
+
 public:
-    // Конструктор с дополнительным параметром - автором
-    AuthorBook(const std::string& title, const std::string& author, 
+    // Конструктор с указателем на автора
+    AuthorBook(const std::string& title, const std::shared_ptr<Author>& author,
                const std::string& theme, const std::string& publisher, 
                const std::string& location, int year);
     
-    // Специфичный геттер для получения автора
-    std::string getAuthor() const;
+    // Конструктор с созданием нового автора (для обратной совместимости)
+    AuthorBook(const std::string& title, const std::string& authorName,
+               const std::string& theme, const std::string& publisher, 
+               const std::string& location, int year);
+
+    // Получение автора
+    std::shared_ptr<Author> getAuthor() const;
     
     // Реализация чисто виртуального метода из базового класса
-    // Возвращает вектор с одним элементом - автором книги
-    std::vector<std::string> getAuthors() const override;
+    std::vector<std::shared_ptr<Author>> getAuthors() const override;
+    
+    // Реализация метода для получения имен авторов
+    std::vector<std::string> getAuthorNames() const override;
     
     // Переопределение виртуального метода для вывода информации
-    // Добавляет информацию об авторе к базовому выводу
     void printInfo() const override;
 };
